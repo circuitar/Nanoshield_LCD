60e0dc4fd72a1dc9a13b8a0a634ad8f8f2709120
Luis Fernando Chavier <lfchavier@circuitar.com.br>
2015-05-22 11:46:04 -0300

    Convert tabs to spaces.

--- Nanoshield_LCD.cpp
+++ Nanoshield_LCD.cpp
@@ -5,8 +5,6 @@ Copyright (c) 2013 Circuitar
 This software is released under the MIT license. See the attached LICENSE file for details.
 */
 
-#include "Arduino.h"
-#include <Wire.h>
 #include "Nanoshield_LCD.h"
 
 #define LCD_I2C_ADDRESS 0x27
@@ -26,17 +24,17 @@ This software is released under the MIT license. See the attached LICENSE file f
 #define RIGHT_TO_LEFT  0x00
 
 Nanoshield_LCD::Nanoshield_LCD(byte a0, byte a1, byte a2) {
-	// Select the three LSBs of the LCD address in the I2C bus
-	i2cAddress = LCD_I2C_ADDRESS;
-	if (!a0) i2cAddress &= 0x01;
-	if (!a1) i2cAddress &= 0x02;
-	if (!a2) i2cAddress &= 0x04;
-	displayMask = DISPLAY_ON;
-	cursorMask = CURSOR_OFF;
-	blinkMask = BLINK_OFF;
-	backlightMask = BACKLIGHT_ON;
-	autoscrollMask = AUTOSCROLL_OFF;
-	directionMask = LEFT_TO_RIGHT;
+  // Select the three LSBs of the LCD address in the I2C bus
+  i2cAddress = LCD_I2C_ADDRESS;
+  if (!a0) i2cAddress &= 0x01;
+  if (!a1) i2cAddress &= 0x02;
+  if (!a2) i2cAddress &= 0x04;
+  displayMask = DISPLAY_ON;
+  cursorMask = CURSOR_OFF;
+  blinkMask = BLINK_OFF;
+  backlightMask = BACKLIGHT_ON;
+  autoscrollMask = AUTOSCROLL_OFF;
+  directionMask = LEFT_TO_RIGHT;
 }
 
 void Nanoshield_LCD::writeRegister(uint8_t addr, uint8_t data)
@@ -49,187 +47,187 @@ void Nanoshield_LCD::writeRegister(uint8_t addr, uint8_t data)
 
 void Nanoshield_LCD::writeLcd4(uint8_t data, bool rs)
 {
-	// Use only lower 4 bits of data
-	data &= 0x0F;
+  // Use only lower 4 bits of data
+  data &= 0x0F;
 
-	// Set to pin GP7 in MCP23008 to high if backlight is on
-	data |= backlightMask;
-	if (rs) data |= 0x40;
-	writeRegister(0x0A, data);
+  // Set to pin GP7 in MCP23008 to high if backlight is on
+  data |= backlightMask;
+  if (rs) data |= 0x40;
+  writeRegister(0x0A, data);
 
-	// Raise clock line
-	data |= 0x10;
-	writeRegister(0x0A, data);
+  // Raise clock line
+  data |= 0x10;
+  writeRegister(0x0A, data);
 
-	// Lower clock line
-	data &= ~0x10;
-	writeRegister(0x0A, data);
+  // Lower clock line
+  data &= ~0x10;
+  writeRegister(0x0A, data);
   delay(1);
 }
 
 void Nanoshield_LCD::writeLcd8(uint8_t data, bool rs)
 {
-	writeLcd4((data >> 4) & 0x0F, rs);
-	writeLcd4(data & 0x0F, rs);
+  writeLcd4((data >> 4) & 0x0F, rs);
+  writeLcd4(data & 0x0F, rs);
 }
 
 void Nanoshield_LCD::displayOnOffControl()
 {
-	writeLcd8(0x08 | displayMask | cursorMask | blinkMask, false);
+  writeLcd8(0x08 | displayMask | cursorMask | blinkMask, false);
 }
 
 void Nanoshield_LCD::entryModeSet()
 {
-	writeLcd8(0x04 | directionMask | autoscrollMask, false);
+  writeLcd8(0x04 | directionMask | autoscrollMask, false);
 }
 
 void Nanoshield_LCD::begin()
 {
-	// Initiate the Wire library and join the I2C bus as a master
+  // Initiate the Wire library and join the I2C bus as a master
   Wire.begin();
-	
-	// Configure all GPIO pins in MCP23008 as outputs and leave everything else as default
-	writeRegister(0x00, 0x00);
-	
-	// Perform LCD reset by instruction in 4-bit mode as described in the HD44780 datasheet
+  
+  // Configure all GPIO pins in MCP23008 as outputs and leave everything else as default
+  writeRegister(0x00, 0x00);
+  
+  // Perform LCD reset by instruction in 4-bit mode as described in the HD44780 datasheet
   delay(50);
-	writeLcd4(0x03, false);
+  writeLcd4(0x03, false);
   delay(5);
-	writeLcd4(0x03, false);
+  writeLcd4(0x03, false);
   delayMicroseconds(100);
-	writeLcd4(0x03, false);
+  writeLcd4(0x03, false);
   delay(5);
-	writeLcd4(0x02, false);
-	writeLcd8(0x28, false);
-	writeLcd8(0x08, false); // Two lines, 5x8 dots per character
-	writeLcd8(0x01, false);
-	writeLcd8(0x06, false); // When writing, increment address and do not shift the display
-	writeLcd8(0x0C, false);
+  writeLcd4(0x02, false);
+  writeLcd8(0x28, false);
+  writeLcd8(0x08, false); // Two lines, 5x8 dots per character
+  writeLcd8(0x01, false);
+  writeLcd8(0x06, false); // When writing, increment address and do not shift the display
+  writeLcd8(0x0C, false);
   delayMicroseconds(100);
 }
 
 void Nanoshield_LCD::clear()
 {
-	writeLcd8(0x01, false);
+  writeLcd8(0x01, false);
 }
 
 void Nanoshield_LCD::home()
 {
-	writeLcd8(0x02, false);
-	delay(1);
+  writeLcd8(0x02, false);
+  delay(1);
 }
 
 void Nanoshield_LCD::setCursor(uint8_t col, uint8_t row)
 {
-	// Get address in the first row
-	uint8_t addr = col & 0x0F;
-	
-	// Add offset if it's not the first row 
-	if (row == 1) {
-		addr += 0x40;
-	}
+  // Get address in the first row
+  uint8_t addr = col & 0x0F;
+  
+  // Add offset if it's not the first row 
+  if (row == 1) {
+    addr += 0x40;
+  }
 
-	// Set DDRAM address
-	writeLcd8(addr | 0x80, false);
+  // Set DDRAM address
+  writeLcd8(addr | 0x80, false);
 }
 
 size_t Nanoshield_LCD::write(uint8_t data)
 {
-	writeLcd8(data, true);
-	return 1;
+  writeLcd8(data, true);
+  return 1;
 }
 
 void Nanoshield_LCD::cursor()
 {
-	cursorMask = CURSOR_ON;
-	displayOnOffControl();
+  cursorMask = CURSOR_ON;
+  displayOnOffControl();
 }
 
 void Nanoshield_LCD::noCursor()
 {
-	cursorMask = CURSOR_OFF;
-	displayOnOffControl();
+  cursorMask = CURSOR_OFF;
+  displayOnOffControl();
 }
 
 void Nanoshield_LCD::blink()
 {
-	blinkMask = BLINK_ON;
-	displayOnOffControl();
+  blinkMask = BLINK_ON;
+  displayOnOffControl();
 }
 
 void Nanoshield_LCD::noBlink()
 {
-	blinkMask = BLINK_OFF;
-	displayOnOffControl();
+  blinkMask = BLINK_OFF;
+  displayOnOffControl();
 }
 
 void Nanoshield_LCD::display()
 {
-	displayMask = DISPLAY_ON;
-	displayOnOffControl();
+  displayMask = DISPLAY_ON;
+  displayOnOffControl();
 }
 
 void Nanoshield_LCD::noDisplay()
 {
-	displayMask = DISPLAY_OFF;
-	displayOnOffControl();
+  displayMask = DISPLAY_OFF;
+  displayOnOffControl();
 }
 
 void Nanoshield_LCD::backlight()
 {
-	backlightMask = BACKLIGHT_ON;
-	writeRegister(0x0A, backlightMask);
+  backlightMask = BACKLIGHT_ON;
+  writeRegister(0x0A, backlightMask);
 }
 
 void Nanoshield_LCD::noBacklight()
 {
-	backlightMask = BACKLIGHT_OFF;
-	writeRegister(0x0A, backlightMask);
+  backlightMask = BACKLIGHT_OFF;
+  writeRegister(0x0A, backlightMask);
 }
 
 void Nanoshield_LCD::scrollDisplayLeft()
 {
-	writeLcd8(0x18, false);
+  writeLcd8(0x18, false);
 }
 
 void Nanoshield_LCD::scrollDisplayRight()
 {
-	writeLcd8(0x1C, false);
+  writeLcd8(0x1C, false);
 }
 
 void Nanoshield_LCD::autoscroll()
 {
-	autoscrollMask = AUTOSCROLL_ON;
-	entryModeSet();
+  autoscrollMask = AUTOSCROLL_ON;
+  entryModeSet();
 }
 
 void Nanoshield_LCD::noAutoscroll()
 {
-	autoscrollMask = AUTOSCROLL_OFF;
-	entryModeSet();
+  autoscrollMask = AUTOSCROLL_OFF;
+  entryModeSet();
 }
 
 void Nanoshield_LCD::leftToRight()
 {
-	directionMask = LEFT_TO_RIGHT;
-	entryModeSet();
+  directionMask = LEFT_TO_RIGHT;
+  entryModeSet();
 }
 
 void Nanoshield_LCD::rightToLeft()
 {
-	directionMask = RIGHT_TO_LEFT;
-	entryModeSet();
+  directionMask = RIGHT_TO_LEFT;
+  entryModeSet();
 }
 
 void Nanoshield_LCD::createChar(uint8_t num, uint8_t data[])
 {
-	if (num > 7) return;
+  if (num > 7) return;
 
-	// Address character position in CGRAM
-	writeLcd8(0x40 | (num << 3), false);
-	
-	// Write character in CGRAM
-	for (int i = 0; i < 8; i++) {
-		writeLcd8(data[i], true);
-	}
+  // Address character position in CGRAM
+  writeLcd8(0x40 | (num << 3), false);
+  
+  // Write character in CGRAM
+  for (int i = 0; i < 8; i++) {
+    writeLcd8(data[i], true);
+  }
 }
--- Nanoshield_LCD.h
+++ Nanoshield_LCD.h
@@ -11,43 +11,43 @@ This software is released under the MIT license. See the attached LICENSE file f
 #include "Arduino.h"
 
 class Nanoshield_LCD: public Print {
-	public:
-		Nanoshield_LCD(byte a0 = 1, byte a1 = 1, byte a2 = 1);
-		void begin();
-		void clear();
-		void home();
-		void setCursor(uint8_t col, uint8_t row); 
-		virtual size_t write(uint8_t);
-		void cursor();
-		void noCursor();
-		void blink();
-		void noBlink();
-		void display();
-		void noDisplay();
-		void backlight();
-		void noBacklight();
-		void scrollDisplayLeft();
-		void scrollDisplayRight();
-		void autoscroll();
-		void noAutoscroll();
-		void leftToRight();
-		void rightToLeft();
-		void createChar(uint8_t, uint8_t[]);
+  public:
+    Nanoshield_LCD(byte a0 = 1, byte a1 = 1, byte a2 = 1);
+    void begin();
+    void clear();
+    void home();
+    void setCursor(uint8_t col, uint8_t row); 
+    virtual size_t write(uint8_t);
+    void cursor();
+    void noCursor();
+    void blink();
+    void noBlink();
+    void display();
+    void noDisplay();
+    void backlight();
+    void noBacklight();
+    void scrollDisplayLeft();
+    void scrollDisplayRight();
+    void autoscroll();
+    void noAutoscroll();
+    void leftToRight();
+    void rightToLeft();
+    void createChar(uint8_t, uint8_t[]);
 
-	private:
-		int8_t i2cAddress;
-		uint8_t cursorMask;
-		uint8_t blinkMask;
-		uint8_t displayMask;
-		uint8_t backlightMask;
-		uint8_t autoscrollMask;
-		uint8_t directionMask;
+  private:
+    int8_t i2cAddress;
+    uint8_t cursorMask;
+    uint8_t blinkMask;
+    uint8_t displayMask;
+    uint8_t backlightMask;
+    uint8_t autoscrollMask;
+    uint8_t directionMask;
 
-		void writeRegister(uint8_t addr, uint8_t data);
-		void writeLcd4(uint8_t data, bool rs);
-		void writeLcd8(uint8_t data, bool rs);
-		void displayOnOffControl();
-		void entryModeSet();
+    void writeRegister(uint8_t addr, uint8_t data);
+    void writeLcd4(uint8_t data, bool rs);
+    void writeLcd8(uint8_t data, bool rs);
+    void displayOnOffControl();
+    void entryModeSet();
 };
 
 #endif
