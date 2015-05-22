f36acd486e168dcad6471a25d1650f1c407cdb17
Luis Fernando Chavier <lfchavier@circuitar.com.br>
2015-05-22 11:48:15 -0300

    Re-add includes that were removed incorrectly.

--- Nanoshield_LCD.cpp
+++ Nanoshield_LCD.cpp
@@ -5,6 +5,8 @@ Copyright (c) 2013 Circuitar
 This software is released under the MIT license. See the attached LICENSE file for details.
 */
 
+#include "Arduino.h"
+#include <Wire.h>
 #include "Nanoshield_LCD.h"
 
 #define LCD_I2C_ADDRESS 0x27
